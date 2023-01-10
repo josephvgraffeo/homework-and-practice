e5a77208c9086363d91a204e68b1480cd25853b6
Joseph Graffeo <jgraff36@Josephs-MacBook-Air.local>
2023-01-10 18:17:32 -0500

    homework for week 1 day 2

---
+++ index.js
@@ -0,0 +1,13 @@
+const country = "United States";
+const continent = "North America";
+
+console.log("My Country: ", country);
+console.log("My Continent: ", continent);
+
+const myDog = "Eli"
+
+console.log("My dog's name is ", myDog);
+
+/*"15" === 15 should return false because they are not the same data type,
+one is a string and one is a number*/
+console.log("15" === 15);
---
+++ variable.js
@@ -0,0 +1,10 @@
+const country = "United States";
+const continent = "North America";
+
+console.log("My country is", country);
+console.log("The " + country, "is in", continent);
+
+const myCar = "2021 Infiniti Q50";
+
+console.log("My car is a ", myCar);
+
