aff2a7e466524622625d23b30572fdb4e388a051
Joseph Graffeo <jgraff36@Josephs-MacBook-Air.local>
2023-01-10 18:43:26 -0500

    finished homework

---
+++ function-homework.js
@@ -0,0 +1,29 @@
+
+// Function adding 2 numbers together
+function addition (x = 0, y = 0) {
+    const sum = x + y
+    console.log(sum)
+    return
+}
+
+console.log ( addition (10, 90) )
+
+
+// Function that squares a number
+function square (x = 0) {
+    const sqr = x**2
+    console.log(sqr)
+    return
+}
+
+console.log( square (57) )
+
+// Object called student with 3 defined variables
+const student = {
+    fullName: "Joseph Graffeo",
+    age: 25,
+    favSportsTeam: "Giants"
+}
+
+console.log(student.favSportsTeam);
+// ^^^ just making sure it works
