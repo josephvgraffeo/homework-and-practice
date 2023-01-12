d21962deb014a2e35480e5722656a74ff25944cd
Joseph Graffeo <jgraff36@Josephs-MacBook-Air.local>
2023-01-11 20:26:17 -0500

    day 3 homework

---
+++ homework-week1-day3.js
@@ -0,0 +1,39 @@
+// Loop that prints a pyramid of choice emoji
+let pyramid = "";
+for (let i = 0; i <= 6; i++) {
+    for (k = 1; k < i; k++) {
+        pyramid += "🌀"
+    }
+    pyramid += "\n";
+}
+console.log(pyramid)
+
+// Function that shifts first term in given array and pushes "kiwi"
+let favoriteFruits = ["mango", "lychee", "rambutan", "papya"]
+
+function kiwiMachine() {
+    favoriteFruits.push("kiwi");
+    favoriteFruits.shift();
+    return favoriteFruits
+}
+console.log(kiwiMachine());
+
+// *Bonus* create a function that returns a letter grade
+function assignGrade(grade) {
+    if (grade > 100) {
+        return "You can get that much extra credit?"
+    } else if (grade >= 90) {
+        return "A";
+    } else if (grade >= 80) {
+        return "B";
+    } else if (grade >= 70) {
+        return "C";
+    } else if (grade >= 60) {
+        return "D";
+    } else if (grade >= 0) {
+        return "F"
+    } else {
+        return "Not a valid grade!"
+    }
+}
+console.log(assignGrade(2000));
