cfe4f48df82f8229b29fc0d82189a0067965cf23
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-01-17 08:57:49 -0500

    fixed switch errors

--- hmwk-w2d1-hundred.js
+++ hmwk-w2d1-hundred.js
@@ -1,35 +1,39 @@
 // // Write a function that prints from 1 to 100. But log “Fizz” instead of the number if it is divisible by 3, “Buzz” if the number is divisible by 5, and “FizzBuzz” if the number is divisible by both 3 and 5.
 
-// function buzzFizz() {
-//     for (i = 1; i < 101; i++) {
-//         switch (true) {
-//             case (i % 3 === true && i % 5 === true):
-//                 console.log("FizzBuzz");
-//             case (i % 3 === true):
-//                 console.log("Fizz");
-//             case (i % 5 === true):
-//                 console.log("Buzz");
-//             default:
-//                 console.log(i)
-//         }
-//     }
-// }
-
-// buzzFizz(); 
-
-// Same function but using else if
-function buzzFizz() {
+function buzzFizzRite() {
     for (i = 1; i < 101; i++) {
-        if (i % 3 ===0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i)
+        switch (true) {
+            case i % 3 === 0 && i % 5 === 0:
+                console.log("FizzBuzz");
+                break;
+            case i % 5 === 0:
+                console.log("Fizz");
+                break;
+            case i % 3 === 0:
+                console.log("Buzz");
+                break;
+            default:
+                console.log(i)
         }
     }
 }
 
-buzzFizz()
+buzzFizzRite(); 
+
+
+// // Same function but using else if
+// function buzzFizz() {
+//     for (i = 1; i < 101; i++) {
+//         if (i % 3 ===0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 5 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 3 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// buzzFizz()
