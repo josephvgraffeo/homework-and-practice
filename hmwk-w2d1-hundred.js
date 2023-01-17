55fd8623da9fb50e70796d2b618eebf630179884
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-01-17 08:04:50 -0500

    tried to fix switch function

--- hmwk-w2d1-hundred.js
+++ hmwk-w2d1-hundred.js
@@ -1,36 +1,35 @@
 // // Write a function that prints from 1 to 100. But log “Fizz” instead of the number if it is divisible by 3, “Buzz” if the number is divisible by 5, and “FizzBuzz” if the number is divisible by both 3 and 5.
 
 // function buzzFizz() {
-//     let counter = 0
 //     for (i = 1; i < 101; i++) {
-//         if (i % 3 ===0 && i % 5 === 0) {
-//             console.log("FizzBuzz");
-//         } else if (i % 3 === 0) {
-//             console.log("Fizz");
-//         } else if (i % 5 === 0) {
-//             console.log("Buzz");
-//         } else {
-//             console.log(i)
+//         switch (true) {
+//             case (i % 3 === true && i % 5 === true):
+//                 console.log("FizzBuzz");
+//             case (i % 3 === true):
+//                 console.log("Fizz");
+//             case (i % 5 === true):
+//                 console.log("Buzz");
+//             default:
+//                 console.log(i)
 //         }
 //     }
 // }
 
-// buzzFizz()
+// buzzFizz(); 
 
+// Same function but using else if
 function buzzFizz() {
-    let counter = 0
     for (i = 1; i < 101; i++) {
-        switch (true) {
-            case (i % 3 === 0 && i % 5 === 0):
-                console.log("FizzBuzz");
-            case (i % 3 === 0):
-                console.log("Fizz");
-            case (i % 5 === 0):
-                console.log("Buzz");
-            default:
-                console.log(i)
+        if (i % 3 ===0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i)
         }
     }
 }
 
-buzzFizz(); 
+buzzFizz()
