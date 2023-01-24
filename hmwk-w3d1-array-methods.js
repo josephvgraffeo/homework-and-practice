ddb64d2b35ebda85c2e3244d88ac23ff59be5e5f
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-01-24 13:36:13 -0500

    updated forEach

--- hmwk-w3d1-array-methods.js
+++ hmwk-w3d1-array-methods.js
@@ -1,11 +1,11 @@
 // Write a function that takes an array of numbers as a parameter and uses .forEach() to double the value of all even numbers in the array
 const numbers = [2,4,6,9,10,11,231,250]
 
-const doubleEvenNums = numbers.forEach( (numbers) => {
-    if (numbers % 2 === 0) {
-        console.log(numbers * 2)
+const doubleEvenNums = numbers.forEach( (element) => {
+    if (element % 2 === 0) {
+        console.log(element * 2)
     } else {
-        console.log(numbers)
+        console.log(element)
     };
 });
 
